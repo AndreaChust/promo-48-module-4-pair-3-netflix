5b5fabf9045315d6156684c671e2df87b221dbfc
AndreaChust <andrea.chust@hotmail.com>
2025-04-14 10:38:47 +0200

    diagramas y relaciones

--- bd/Netflix.sql
+++ bd/Netflix.sql
@@ -1,22 +1,13 @@
-CREATE DATABASE netflix;
-
-USE netflix; 
+CREATE DATABASE pruebas;
+USE pruebas;
 
 CREATE TABLE movies (
 	idMovie INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    title VARCHAR(45) NOT NULL,
+	title VARCHAR(45) NOT NULL,
     genre VARCHAR(45) NOT NULL,
     image VARCHAR(1000) NOT NULL,
     category VARCHAR(45) NOT NULL, 
     year INT
-);
-
-CREATE TABLE users (
-	idUser INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    user VARCHAR(45) NOT NULL,
-    password VARCHAR(45) NOT NULL,
-    email VARCHAR(45) NOT NULL,
-    plan_details VARCHAR(45) NOT NULL
 );
 
 CREATE TABLE actors (
@@ -27,6 +18,14 @@ CREATE TABLE actors (
     birthday DATE
 );
 
+CREATE TABLE users (
+	idUser INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    user VARCHAR(45) NOT NULL,
+    password VARCHAR(45) NOT NULL,
+    email VARCHAR(45) NOT NULL,
+    plan_details VARCHAR(45) NOT NULL
+);
+
 SELECT * FROM movies;
 
 INSERT INTO  movies (title, genre, image, category, year)
@@ -35,11 +34,8 @@ VALUES
 	('La vita è bella', 'Comedia', 'https://pics.filmaffinity.com/la_vita_e_bella-646167341-mmed.jpg', 'Top 10', 1996),
     ('Forrest Gump', 'Comedia', 'https://pics.filmaffinity.com/forrest_gump-212765827-mmed.jpg', 'Top 10', 1994);
     
-    
-ALTER TABLE users ADD COLUMN name VARCHAR(45) NOT NULL;    
+ALTER TABLE users ADD COLUMN name VARCHAR(45) NOT NULL; 
 
-SELECT * FROM users; 
-    
 INSERT INTO users (user, password, name, email, plan_details)
 VALUES
 	('laura_dev', 'laura', 'Laura', 'laura@gmail.com', 'Standard'),
@@ -51,14 +47,30 @@ VALUES
 	('Tom', 'Hanks', 'Estados Unidos', '1956-07-09'),
 	('Roberto', 'Benigni', 'Italia', '1952-10-27'),
 	('John', 'Travolta', 'Estados Unidos', '1954-02-18');
-    
 
-SELECT * FROM actors WHERE birthday > '1954-01-01' AND birthday < '1960-12-31';
-SELECT * FROM users; 
+SELECT * FROM actors;
 
-SELECT name, lastname  FROM actors WHERE country = 'Estados Unidos';
-UPDATE movies SET year = 1997 WHERE idMovie = 2;
+DELETE FROM users WHERE idUser = 4;
+DELETE FROM users WHERE idUser = 5;
+DELETE FROM users WHERE idUser = 6;
+DELETE FROM actors WHERE idActor = 4;
+DELETE FROM actors WHERE idActor = 5;
+DELETE FROM actors WHERE idActor = 6;
 
-SET SQL_SAFE_UPDATES = 0;
-DELETE FROM users WHERE name LIKE 'M%';
-SET SQL_SAFE_UPDATES = 1;
+SELECT * FROM movies_has_users;
+
+INSERT INTO movies_has_users (fk_users, fk_movies)
+VALUES 
+	(1, 1),
+    (1, 2),
+    (2, 2);
+
+SELECT users.user, movies.title
+FROM users, movies, movies_has_users
+WHERE movies_has_users.fk_movies = movies.idMovie
+AND movies_has_users.fk_users = users.idUser;
+
+
+
+
+
